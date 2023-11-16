d797d9b141b1631107026e2aca1c4b02f52ee850
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-11-16 12:31:43 -0300

    Modificaciones

--- TPFinal/src/Escritorio.java
+++ TPFinal/src/Escritorio.java
@@ -54,7 +54,6 @@ public class Escritorio extends javax.swing.JFrame {
         if(usuario.equals("ADMIN")){
             TablasInformacion tablaClientes = new TablasInformacion("clientes");
             tablaClientes.setVisible(true);
-            jPanel_escritorio.add(tablaClientes);
         }else{
             JOptionPane.showMessageDialog(null, "No tiene acceso a está del código");
         }
